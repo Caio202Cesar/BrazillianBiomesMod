631113367fe0fa046e61c7d7899e8e18a8b85a13
Caio202Cesar <caiodnobre@gmail.com>
2024-10-31 06:18:56 -0300

    Second Biome EXCLUDED.

--- src/main/java/com/brbiomesmod/world/biomes/Util/ModBiomeGeneration.java
+++ src/main/java/com/brbiomesmod/world/biomes/Util/ModBiomeGeneration.java
@@ -1,7 +1,6 @@
 package com.brbiomesmod.world.biomes.Util;
 
 import com.brbiomesmod.world.biomes.AmazonRainforestBiome;
-import com.brbiomesmod.world.biomes.PantanalBiome;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
@@ -11,14 +10,12 @@ import net.minecraftforge.registries.ForgeRegistries;
 import java.util.Objects;
 
 import static net.minecraftforge.common.BiomeDictionary.Type.*;
-import static net.minecraftforge.common.BiomeDictionary.Type.CONIFEROUS;
 
 public class ModBiomeGeneration {
     public static void generateBiomes() {
         addBiome(AmazonRainforestBiome.AMAZON_RAINFOREST.get(), BiomeManager.BiomeType.WARM, 26, DENSE, FOREST,
                 HOT, JUNGLE, LUSH, WET);
-        addBiome(PantanalBiome.PANTANAL.get(), BiomeManager.BiomeType.WARM, 26, SWAMP, SPARSE,
-                HOT, LUSH, WET, PLAINS, SAVANNA, RARE);
+
     }
 
     private static void addBiome(Biome biome, BiomeManager.BiomeType type, int weight, BiomeDictionary.Type... types) {
