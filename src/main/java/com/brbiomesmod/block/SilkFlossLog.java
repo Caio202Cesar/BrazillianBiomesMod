3c2a40935df7d012f46a5c5df34ddf6fd1c1777a
Caio202Cesar <caiodnobre@gmail.com>
2024-12-02 00:11:51 -0300

    Brazilnut fruit now makes a sound when broken.; increased fruiting cambuci leaves texture

--- src/main/java/com/brbiomesmod/block/SilkFlossLog.java
+++ src/main/java/com/brbiomesmod/block/SilkFlossLog.java
@@ -1,12 +1,16 @@
 package com.brbiomesmod.block;
 
 import net.minecraft.block.*;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.pathfinding.PathNodeType;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IPlantable;
@@ -15,6 +19,9 @@ import net.minecraftforge.common.ToolType;
 import javax.annotation.Nullable;
 
 public class SilkFlossLog extends RotatedPillarBlock {
+    protected static final VoxelShape COLLISION_SHAPE = Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 15.0D, 15.0D);
+    protected static final VoxelShape OUTLINE_SHAPE = Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D);
+
     public SilkFlossLog() {
         super(AbstractBlock.Properties.from(Blocks.OAK_LOG).sound(SoundType.WOOD).hardnessAndResistance(2.0f)
                 .harvestTool(ToolType.AXE));
@@ -33,6 +40,16 @@ public class SilkFlossLog extends RotatedPillarBlock {
     }
 
     @SuppressWarnings("deprecation")
+    public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return COLLISION_SHAPE;
+    }
+
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return OUTLINE_SHAPE;
+    }
+
+    @SuppressWarnings("deprecation")
+
     public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
         entityIn.attackEntityFrom(DamageSource.CACTUS, 1.0F);
     }
