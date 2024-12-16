3e18c440548d3901d463d63d66990cac1ce25435
Caio202Cesar <caiodnobre@gmail.com>
2024-12-16 00:15:48 -0300

    Added Sapucaia and Cockspur Coral Trees.

---
+++ src/main/java/com/brbiomesmod/block/Saplings/CockspurCoralSapling.java
@@ -0,0 +1,48 @@
+package com.brbiomesmod.block.Saplings;
+
+import com.brbiomesmod.block.BlockClasses.PampasPlainsBlocks;
+import com.brbiomesmod.features.TreeFeatures;
+import net.minecraft.block.*;
+import net.minecraft.block.trees.Tree;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.Random;
+
+public class CockspurCoralSapling extends SaplingBlock {
+    public CockspurCoralSapling() {
+        super(new CockspurCoralSapling.CockspurCoralTree(), AbstractBlock.Properties.from(Blocks.OAK_SAPLING).hardnessAndResistance(0.0f)
+                .sound(SoundType.PLANT));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void registerRenderLayer() {
+        RenderTypeLookup.setRenderLayer(PampasPlainsBlocks.COCKSPUR_CORAL_SAPLING.get(), RenderType.getCutout());
+    }
+
+    public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+        return 80;
+    }
+
+    public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+        return 60;
+    }
+
+
+    private static class CockspurCoralTree extends Tree {
+        @Override
+        protected ConfiguredFeature<BaseTreeFeatureConfig, ?> getTreeFeature(Random random, boolean p_225546_2_) {
+            if (random.nextInt(10) == 8) {
+                return TreeFeatures.COCKSPUR_CORAL_TREE2;
+            } else {
+                return TreeFeatures.COCKSPUR_CORAL_TREE2;
+            }}
+    }
+}
