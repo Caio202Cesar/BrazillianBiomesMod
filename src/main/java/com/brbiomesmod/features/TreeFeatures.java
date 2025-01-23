0c918bfabab046de8edf068abeeae7801e00b789
Caio202Cesar <caiodnobre@gmail.com>
2025-01-23 11:45:56 -0300

    Changes in Cerrado and Amazon trees generation.

--- src/main/java/com/brbiomesmod/features/TreeFeatures.java
+++ src/main/java/com/brbiomesmod/features/TreeFeatures.java
@@ -646,7 +646,7 @@ public abstract class TreeFeatures implements IFeatureConfig {
     }
 
     public static final ConfiguredFeature<?, ?> AMAZON_TREES = register("amazon_trees",
-            Feature.RANDOM_SELECTOR.withConfiguration(new MultipleRandomFeatureConfig(ImmutableList.of(KAPOK_TREE.withChance(0.4F),
+            Feature.RANDOM_SELECTOR.withConfiguration(new MultipleRandomFeatureConfig(ImmutableList.of(KAPOK_TREE.withChance(0.6F),
                             CUPUACU_TREE.withChance(0.1F), CAMU_CAMU_TREE.withChance(0.2F)), SHARINGA_TREE)).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT)
                     .withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(6, 0.1F, 1))));
 
