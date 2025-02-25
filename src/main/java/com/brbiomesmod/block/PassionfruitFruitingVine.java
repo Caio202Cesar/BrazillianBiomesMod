a8ef26bdec870c56ac7563a6358e07952c830095
Caio202Cesar <caiodnobre@gmail.com>
2025-02-24 21:17:26 -0300

    The fruiting passionfruit vine now drops a passion fruit item when ticks to non fruiting vine.

--- src/main/java/com/brbiomesmod/block/PassionfruitFruitingVine.java
+++ src/main/java/com/brbiomesmod/block/PassionfruitFruitingVine.java
@@ -15,13 +15,55 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.PlantType;
 import net.minecraftforge.common.ToolType;
+
+import java.util.Random;
 
 public class PassionfruitFruitingVine extends VineBlock {
     public PassionfruitFruitingVine() {
         super(Properties.from(Blocks.VINE).tickRandomly().zeroHardnessAndResistance()
                 .sound(SoundType.PLANT).doesNotBlockMovement().notSolid().harvestTool(ToolType.HOE));
+    }
+
+    public boolean ticksRandomly(BlockState state) {
+        return true;
+    }
+
+    /**
+     * Performs a random tick on a block.
+     *
+     * @param state
+     * @param worldIn
+     * @param pos
+     * @param random
+     */
+    @Override
+    public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
+        super.randomTick(state, worldIn, pos, random);
+
+        int dropCount = 1;
+
+        ItemStack itemStack = new ItemStack(ModItems.PASSIONFRUIT.get(), dropCount);
+        ItemEntity itemEntity = new ItemEntity(worldIn, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, itemStack);
+
+        worldIn.addEntity(itemEntity);
+
+        double chance = 0.2;
+
+        if (random.nextDouble() < chance) {
+
+            BlockState currentState = state;
+            BlockState newState = AtlanticForestBlocks.PASSION_FRUIT_VINE.get().getDefaultState();
+
+            worldIn.setBlockState(pos, AtlanticForestBlocks.PASSION_FRUIT_VINE.get().getDefaultState());
+
+            newState = newState.with(VineBlock.NORTH, currentState.get(VineBlock.NORTH)).with(VineBlock.EAST, currentState.get(VineBlock.EAST))
+                    .with(VineBlock.SOUTH, currentState.get(VineBlock.SOUTH)).with(VineBlock.WEST, currentState.get(VineBlock.WEST));
+
+            worldIn.setBlockState(pos, newState, 3);
+        }
     }
 
     @Override
