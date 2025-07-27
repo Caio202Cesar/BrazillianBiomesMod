0d01a95cb95ec6186ad11b3d4b9db8bb0387e986
ADM Caio <caiodnobre@gmail.com>
2025-07-26 22:01:25 -0300

    Removed non-BR natives, like calabash and banana. They will be added to Caio Cesar's biomes.

--- src/main/java/com/brbiomesmod/events/ModEventBusEvents.java
+++ src/main/java/com/brbiomesmod/events/ModEventBusEvents.java
@@ -2,8 +2,6 @@ package com.brbiomesmod.events;
 
 import com.brbiomesmod.BrazillianBiomesMod;
 import com.brbiomesmod.events.loot.ApricotIpeStructureAdditionModifier;
-import com.brbiomesmod.events.loot.CalabashSeedsAdditionModifier;
-import com.brbiomesmod.events.loot.CalabashSeedsStructureAdditionModifier;
 import com.brbiomesmod.events.loot.MateCupStructureAdditionModifier;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
@@ -20,16 +18,6 @@ public class ModEventBusEvents {
     public static void registerModifierSerializers(@Nonnull final RegistryEvent.Register<GlobalLootModifierSerializer<?>>
                                                            event) {
         event.getRegistry().registerAll(
-                new CalabashSeedsAdditionModifier.Serializer().setRegistryName
-                        (new ResourceLocation(BrazillianBiomesMod.MOD_ID,"calabash_seeds_from_grass")),
-                new CalabashSeedsAdditionModifier.Serializer().setRegistryName
-                        (new ResourceLocation(BrazillianBiomesMod.MOD_ID,"calabash_seeds_from_tall_grass")),
-                new CalabashSeedsStructureAdditionModifier.Serializer().setRegistryName
-                        (new ResourceLocation(BrazillianBiomesMod.MOD_ID,"calabash_seeds_in_abandoned_mineshaft")),
-                new CalabashSeedsStructureAdditionModifier.Serializer().setRegistryName
-                        (new ResourceLocation(BrazillianBiomesMod.MOD_ID,"calabash_seeds_in_dungeon")),
-                new CalabashSeedsStructureAdditionModifier.Serializer().setRegistryName
-                        (new ResourceLocation(BrazillianBiomesMod.MOD_ID,"calabash_seeds_in_stronghold")),
                 new MateCupStructureAdditionModifier.Serializer().setRegistryName
                         (new ResourceLocation(BrazillianBiomesMod.MOD_ID,"mate_cup_in_bonus_chest")),
                 new MateCupStructureAdditionModifier.Serializer().setRegistryName
