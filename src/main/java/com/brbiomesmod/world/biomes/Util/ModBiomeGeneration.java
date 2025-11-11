69ea7e3334a4f2ecd67a8f5a56befc625f9ab3af
ADM Caio <caiodnobre@gmail.com>
2025-11-11 19:10:08 -0300

    Changed guarana fruit and seeds textures.

--- src/main/java/com/brbiomesmod/world/biomes/Util/ModBiomeGeneration.java
+++ src/main/java/com/brbiomesmod/world/biomes/Util/ModBiomeGeneration.java
@@ -13,24 +13,20 @@ import static net.minecraftforge.common.BiomeDictionary.Type.*;
 
 public class ModBiomeGeneration {
     public static void generateBiomes() {
-        addBiome(AmazonRainforestBiome.AMAZON_RAINFOREST.get(), BiomeManager.BiomeType.DESERT, 27, OVERWORLD, HOT, DENSE, FOREST,
-                JUNGLE, LUSH, WET, SPOOKY);
-        addBiome(AraucariaPlateauBiome.ARAUCARIA_PLATEAU.get(), BiomeManager.BiomeType.WARM, 18, OVERWORLD, DENSE, FOREST, PLATEAU,
-                RARE, CONIFEROUS, LUSH, WET, MOUNTAIN);
-        addBiome(PantanalBiome.PANTANAL.get(), BiomeManager.BiomeType.DESERT, 18, OVERWORLD, HOT, SPARSE, SAVANNA, RARE, WET, SWAMP,
-                PLAINS, LUSH);
+        addBiome(AmazonRainforestBiome.AMAZON_RAINFOREST.get(), BiomeManager.BiomeType.DESERT, 27, OVERWORLD, HOT, JUNGLE);
+        addBiome(AraucariaPlateauBiome.ARAUCARIA_PLATEAU.get(), BiomeManager.BiomeType.WARM, 25, OVERWORLD, PLATEAU);
+        addBiome(PantanalBiome.PANTANAL.get(), BiomeManager.BiomeType.DESERT, 18, OVERWORLD, HOT, SPARSE, SAVANNA);
         addBiome(CaatingaBiome.CAATINGA.get(), BiomeManager.BiomeType.DESERT, 25, OVERWORLD, HOT, SPARSE, SAVANNA, DRY, SANDY);
-        addBiome(DunasRestingaBiome.DUNAS_RESTINGA.get(), BiomeManager.BiomeType.DESERT, 25, OVERWORLD, HOT, DENSE, SANDY);
+        addBiome(DunasRestingaBiome.DUNAS_RESTINGA.get(), BiomeManager.BiomeType.DESERT, 25, OVERWORLD, HOT, SANDY);
         addBiome(PampasBiome.PAMPAS.get(), BiomeManager.BiomeType.WARM, 25, OVERWORLD, SPARSE, PLAINS);
-        addBiome(ButiaPampasBiome.BUTIA_PAMPAS.get(), BiomeManager.BiomeType.WARM, 25, OVERWORLD, SPARSE, PLAINS);
+        addBiome(ButiaPampasBiome.BUTIA_PAMPAS.get(), BiomeManager.BiomeType.WARM, 25, OVERWORLD, PLAINS);
         addBiome(PampasCoastalFields.PAMPAS_COASTAL_FIELDS.get(), BiomeManager.BiomeType.WARM, 25, OVERWORLD, SPARSE, PLAINS, BEACH, SANDY);
         addBiome(CerradoBiome.CERRADO.get(), BiomeManager.BiomeType.DESERT, 25, HOT, OVERWORLD, SPARSE, SAVANNA);
         addBiome(RestingaBeachBiome.RESTINGA_BEACH.get(), BiomeManager.BiomeType.DESERT, 25, OVERWORLD, HOT, BEACH, SANDY);
-        addBiome(AtlanticForestBiome.ATLANTIC_FOREST.get(), BiomeManager.BiomeType.DESERT, 25, OVERWORLD, HOT, DENSE, FOREST, LUSH);
-        addBiome(AtlanticForestHillsBiome.ATLANTIC_FOREST_HILLS.get(), BiomeManager.BiomeType.DESERT, 18, OVERWORLD, HOT, DENSE, WET, HILLS,
-                MAGICAL, FOREST, LUSH);
-        addBiome(AmazonVarzeaForestBiome.AMAZON_VARZEA_FOREST.get(), BiomeManager.BiomeType.DESERT, 23, OVERWORLD, HOT, DENSE, FOREST,
-                JUNGLE, LUSH, WET, SWAMP);
+        addBiome(AtlanticForestBiome.ATLANTIC_FOREST.get(), BiomeManager.BiomeType.DESERT, 25, OVERWORLD, HOT, DENSE, LUSH, JUNGLE);
+        addBiome(AtlanticForestHillsBiome.ATLANTIC_FOREST_HILLS.get(), BiomeManager.BiomeType.DESERT, 18, OVERWORLD, HOT, DENSE, HILLS, JUNGLE,
+                MAGICAL);
+        addBiome(AmazonVarzeaForestBiome.AMAZON_VARZEA_FOREST.get(), BiomeManager.BiomeType.DESERT, 23, OVERWORLD, HOT, JUNGLE);
 
 
     }
