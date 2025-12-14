fbe104185c97abb85472cc67e14b076d61504e87
ADM Caio <caiodnobre@gmail.com>
2025-12-14 18:28:45 -0300

    Added seasonality to mangaba tree and solved the mangaba overfruiting problem.

--- src/main/java/com/brbiomesmod/block/Custom/Leaves/PitangaLeaves.java
+++ src/main/java/com/brbiomesmod/block/Custom/Leaves/PitangaLeaves.java
@@ -40,7 +40,7 @@ public class PitangaLeaves extends LeavesBlock implements IForgeShearable {
         Biome biome = worldIn.getBiome(pos);
         float temp = biome.getTemperature(pos);
 
-        //If biome is coller (< 0.85 MC temp - Hardiness zone 9) AND it's WINTER, change to winter leaves ===
+        //If biome is warmer (< 0.85 MC temp - Hardiness zone 9) AND it's WINTER, change to winter leaves ===
         if (temp < 0.84F && "WINTER".equals(currentSeason) && random.nextInt(5) == 0) {
             int distance = state.get(LeavesBlock.DISTANCE);
             boolean persistent = state.get(LeavesBlock.PERSISTENT);
