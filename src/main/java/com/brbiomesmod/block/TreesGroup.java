f55b51e1bca53eac560652568c8435479c003ae4
ADM Caio <caiodnobre@gmail.com>
2025-12-19 16:54:47 -0300

    Added a new subbiome for pampas: Coxilhas or Pampas Hills.

--- src/main/java/com/brbiomesmod/block/TreesGroup.java
+++ src/main/java/com/brbiomesmod/block/TreesGroup.java
@@ -175,7 +175,7 @@ public class TreesGroup {
     public static final RegistryObject<Block> WHITE_IPE_LEAVES = registerBlock("white_ipe_leaves",
             () -> new WhiteIpeLeaves(AbstractBlock.Properties.create(Material.LEAVES).hardnessAndResistance(0.2F).tickRandomly()
                     .notSolid().sound(SoundType.PLANT).harvestTool(ToolType.HOE), TreesGroup.WHITE_IPE_FALL_LEAVES));
-    public static final RegistryObject<Block> WHITE_IPE_FALL_LEAVES = registerBlock("apricot_ipe_fall_leaves",
+    public static final RegistryObject<Block> WHITE_IPE_FALL_LEAVES = registerBlock("white_ipe_fall_leaves",
             () -> new WhiteIpeFallLeaves(AbstractBlock.Properties.create(Material.LEAVES).hardnessAndResistance(0.2F).tickRandomly()
                     .notSolid().sound(SoundType.PLANT).harvestTool(ToolType.HOE)));
     public static final RegistryObject<Block> WHITE_IPE_BLOSSOM = registerBlock("white_ipe_blossom",
