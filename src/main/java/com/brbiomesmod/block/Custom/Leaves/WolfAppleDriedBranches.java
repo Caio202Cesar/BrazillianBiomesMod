df78b5cac8215f1f6c2a08f93fb08c40d6c3f8f4
ADM Caio <caiodnobre@gmail.com>
2026-01-14 17:48:55 -0300

    Fixed cashew juice creative category.

---
+++ src/main/java/com/brbiomesmod/block/Custom/Leaves/WolfAppleDriedBranches.java
@@ -0,0 +1,69 @@
+package com.brbiomesmod.block.Custom.Leaves;
+
+import com.brbiomesmod.Seasons.Season;
+import com.brbiomesmod.block.TreesGroup;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.IForgeShearable;
+
+import java.util.Random;
+
+public class WolfAppleDriedBranches extends LeavesBlock implements IForgeShearable {
+     public WolfAppleDriedBranches(Properties properties) {
+         super(properties);
+
+     }
+
+    public boolean ticksRandomly(BlockState state) {
+        return true;
+    }
+
+    /**
+     * Performs a random tick on a block.
+     *
+     * @param state
+     * @param worldIn
+     * @param pos
+     * @param random
+     */
+    @Override
+    public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
+        String currentSeason = Season.getSeason(worldIn.getDayTime());
+
+        if ("WINTER".equals(currentSeason) && random.nextInt(15) == 0) {
+            int distance = state.get(LeavesBlock.DISTANCE);
+            boolean persistent = state.get(LeavesBlock.PERSISTENT);
+
+            worldIn.setBlockState(pos, TreesGroup.WOLF_APPLE_LEAVES.get()
+                    .getDefaultState().with(LeavesBlock.DISTANCE, distance).with(LeavesBlock.PERSISTENT, persistent), 3);
+        }
+
+        if ("SPRING".equals(currentSeason) && random.nextInt(2) == 0) {
+            int distance = state.get(LeavesBlock.DISTANCE);
+            boolean persistent = state.get(LeavesBlock.PERSISTENT);
+
+            worldIn.setBlockState(pos, TreesGroup.WOLF_APPLE_LEAVES.get()
+                    .getDefaultState().with(LeavesBlock.DISTANCE, distance).with(LeavesBlock.PERSISTENT, persistent), 3);
+        }
+
+        if (random.nextInt(65) == 0) {
+            int distance = state.get(LeavesBlock.DISTANCE);
+            boolean persistent = state.get(LeavesBlock.PERSISTENT);
+
+            worldIn.setBlockState(pos, TreesGroup.WOLF_APPLE_FLOWERING_DRIED_BRANCHES.get()
+                    .getDefaultState().with(LeavesBlock.DISTANCE, distance).with(LeavesBlock.PERSISTENT, persistent), 3);
+        }
+    }
+
+    public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+        return 90;
+    }
+
+    public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+        return 100;
+    }
+}
