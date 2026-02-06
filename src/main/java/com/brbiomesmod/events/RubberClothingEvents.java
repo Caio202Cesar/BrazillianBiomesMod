8480a0dd3243eb5830cd9147d7b8da12376aac81
ADM Caio <caiodnobre@gmail.com>
2026-02-05 21:48:48 -0300

    Due to bugs, passionfruit fruiting vines will not drop their fruit anymore.

--- src/main/java/com/brbiomesmod/events/RubberClothingEvents.java
+++ src/main/java/com/brbiomesmod/events/RubberClothingEvents.java
@@ -1,9 +1,17 @@
 package com.brbiomesmod.events;
 
 import com.brbiomesmod.item.ModItems;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EnderPearlEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.projectile.SnowballEntity;
+import net.minecraft.entity.projectile.TridentEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,6 +20,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = "brbiomesmod")
 public class RubberClothingEvents {
 
+    //Rubber boots and leggings absorb damage fall
     @SubscribeEvent
     public static void onLivingFall(LivingFallEvent event) {
 
@@ -43,46 +52,72 @@ public class RubberClothingEvents {
         }
     }
 
+    private static boolean isFullRubber(PlayerEntity player) {
+
+        return player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem() == ModItems.RUBBER_HELMET.get()
+                && player.getItemStackFromSlot(EquipmentSlotType.CHEST).getItem() == ModItems.RUBBER_CHESTPLATE.get()
+                && player.getItemStackFromSlot(EquipmentSlotType.LEGS).getItem() == ModItems.RUBBER_LEGGINGS.get()
+                && player.getItemStackFromSlot(EquipmentSlotType.FEET).getItem() == ModItems.RUBBER_BOOTS.get();
+    }
+
     @SubscribeEvent
     public static void onLivingHurt(LivingHurtEvent event) {
-
         if (!(event.getEntityLiving() instanceof PlayerEntity)) return;
 
+        Entity src = event.getSource().getImmediateSource();
         PlayerEntity player = (PlayerEntity) event.getEntityLiving();
 
-        // Check projectile damage
-        if (!event.getSource().isProjectile()) return;
+        //Prevent damage from hot floor and magma blocks
+        if (event.getSource() != DamageSource.HOT_FLOOR) return;
 
-        ItemStack chest = player.getItemStackFromSlot(EquipmentSlotType.CHEST);
-        ItemStack helmet = player.getItemStackFromSlot(EquipmentSlotType.HEAD);
-        ItemStack leggings = player.getItemStackFromSlot(EquipmentSlotType.LEGS);
+        ItemStack boots = player.getItemStackFromSlot(EquipmentSlotType.FEET);
 
-        if (chest.getItem() != ModItems.RUBBER_CHESTPLATE.get()) return;
+        if (boots.getItem() == ModItems.RUBBER_BOOTS.get()) {
 
-        // Cancel damage
+            // cancel magma damage
+            event.setCanceled(true);
+        }
+
+        //Full rubber armor absorbs certain projectiles damage
+        boolean allowedProjectile =
+                src instanceof AbstractArrowEntity
+                        || src instanceof SnowballEntity
+                        || src instanceof EnderPearlEntity;
+
+        if (!allowedProjectile) return;
+        if (src instanceof TridentEntity) return;
+
+        if (!isFullRubber(player)) return;
+
+        double relativeHitY = src.getPosY() - player.getPosY();
+        double percent = relativeHitY / player.getHeight();
+
+        EquipmentSlotType slotHit;
+
+        if (percent > 0.75) slotHit = EquipmentSlotType.HEAD;
+        else if (percent > 0.35) slotHit = EquipmentSlotType.CHEST;
+        else if (percent > 0.15) slotHit = EquipmentSlotType.LEGS;
+        else slotHit = EquipmentSlotType.FEET;
+
+        ItemStack armor = player.getItemStackFromSlot(slotHit);
+
+        boolean isRubberPiece =
+                (slotHit == EquipmentSlotType.HEAD && armor.getItem() == ModItems.RUBBER_HELMET.get())
+                        || (slotHit == EquipmentSlotType.CHEST && armor.getItem() == ModItems.RUBBER_CHESTPLATE.get())
+                        || (slotHit == EquipmentSlotType.LEGS && armor.getItem() == ModItems.RUBBER_LEGGINGS.get())
+                        || (slotHit == EquipmentSlotType.FEET && armor.getItem() == ModItems.RUBBER_BOOTS.get());
+
+        if (!isRubberPiece) return;
+
         event.setAmount(0F);
 
-        // Damage armor durability
-        int extraDamage = 4; // tweak balance here
-        chest.damageItem(extraDamage, player,
-                (p) -> p.sendBreakAnimation(EquipmentSlotType.CHEST));
+        armor.damageItem(6, player,
+                (p) -> p.sendBreakAnimation(slotHit));
 
-        if (helmet.getItem() != ModItems.RUBBER_HELMET.get()) return;
+        player.world.playSound(null, player.getPosition(),
+                SoundEvents.BLOCK_BONE_BLOCK_HIT,
+                SoundCategory.PLAYERS, 1F, 1F);
 
-        // Cancel damage
-        event.setAmount(0F);
-
-        // Damage armor durability
-        helmet.damageItem(extraDamage, player,
-                (p) -> p.sendBreakAnimation(EquipmentSlotType.HEAD));
-
-        if (leggings.getItem() != ModItems.RUBBER_LEGGINGS.get()) return;
-
-        // Cancel damage
-        event.setAmount(0F);
-
-        // Damage armor durability
-        leggings.damageItem(extraDamage, player,
-                (p) -> p.sendBreakAnimation(EquipmentSlotType.LEGS));
+        src.remove();
     }
 }
