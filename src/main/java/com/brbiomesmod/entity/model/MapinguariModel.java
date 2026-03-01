ab0a4d630c6dbeaddfe2c9c353fd25112fd5abbd
ADM Caio <caiodnobre@gmail.com>
2026-03-01 19:03:50 -0300

    Updated rubber blocks' properties, specially raw rubber block.

--- src/main/java/com/brbiomesmod/entity/model/MapinguariModel.java
+++ src/main/java/com/brbiomesmod/entity/model/MapinguariModel.java
@@ -9,7 +9,6 @@ import net.minecraft.util.math.MathHelper;
 
 public class MapinguariModel<T extends MapinguariEntity> extends EntityModel<T> {
     private final ModelRenderer body;
-    private final ModelRenderer head;
     private final ModelRenderer right_arm;
     private final ModelRenderer left_arm;
     private final ModelRenderer right_leg;
@@ -20,7 +19,7 @@ public class MapinguariModel<T extends MapinguariEntity> extends EntityModel<T> 
         textureHeight = 128;
 
         body = new ModelRenderer(this);
-        body.setRotationPoint(0.0F, 17.0F, 0.0F); // -7.0F offset converted
+        body.setRotationPoint(0.0F, -7.0F, 0.0F); // -7.0F offset converted
         body.setTextureOffset(0, 40)
                 .addBox(-9.0F, -2.0F, -6.0F,
                         18.0F, 12.0F, 11.0F,
@@ -31,13 +30,9 @@ public class MapinguariModel<T extends MapinguariEntity> extends EntityModel<T> 
                         9.0F, 5.0F, 6.0F,
                         0.5F);
 
-        // ================= HEAD (EMPTY, HEADLESS) =================
-        head = new ModelRenderer(this);
-        head.setRotationPoint(0.0F, 17.0F, -2.0F);
-
         // ================= RIGHT ARM =================
         right_arm = new ModelRenderer(this);
-        right_arm.setRotationPoint(0.0F, 17.0F, 0.0F);
+        right_arm.setRotationPoint(0.0F, -7.0F, 0.0F);
         right_arm.setTextureOffset(60, 21)
                 .addBox(-13.0F, -2.5F, -3.0F,
                         4.0F, 30.0F, 6.0F,
@@ -45,7 +40,7 @@ public class MapinguariModel<T extends MapinguariEntity> extends EntityModel<T> 
 
         // ================= LEFT ARM =================
         left_arm = new ModelRenderer(this);
-        left_arm.setRotationPoint(0.0F, 17.0F, 0.0F);
+        left_arm.setRotationPoint(0.0F, -7.0F, 0.0F);
         left_arm.setTextureOffset(60, 58)
                 .addBox(9.0F, -2.5F, -3.0F,
                         4.0F, 30.0F, 6.0F,
@@ -53,7 +48,7 @@ public class MapinguariModel<T extends MapinguariEntity> extends EntityModel<T> 
 
         // ================= RIGHT LEG =================
         right_leg = new ModelRenderer(this);
-        right_leg.setRotationPoint(-4.0F, 11.0F, 0.0F);
+        right_leg.setRotationPoint(-5F, 11F, 0F);
         right_leg.setTextureOffset(37, 0)
                 .addBox(-3.5F, -3.0F, -3.0F,
                         6.0F, 16.0F, 5.0F,
@@ -61,7 +56,7 @@ public class MapinguariModel<T extends MapinguariEntity> extends EntityModel<T> 
 
         // ================= LEFT LEG =================
         left_leg = new ModelRenderer(this);
-        left_leg.setRotationPoint(5.0F, 11.0F, 0.0F);
+        left_leg.setRotationPoint(4F, 11F, 0F);
         left_leg.setTextureOffset(60, 0)
                 .addBox(-3.5F, -3.0F, -3.0F,
                         6.0F, 16.0F, 5.0F,
@@ -107,7 +102,6 @@ public class MapinguariModel<T extends MapinguariEntity> extends EntityModel<T> 
     @Override
     public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
         body.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
-        head.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
         right_arm.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
         left_arm.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
         right_leg.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
