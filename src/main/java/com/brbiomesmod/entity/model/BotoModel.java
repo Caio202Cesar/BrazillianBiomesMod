1e47a0b8e5376bea622872b1d6c3b15b5d3cba35
ADM Caio <caiodnobre@gmail.com>
2026-03-01 10:45:27 -0300

    Updated rubber blocks' properties, specially raw rubber block.

--- src/main/java/com/brbiomesmod/entity/model/BotoModel.java
+++ src/main/java/com/brbiomesmod/entity/model/BotoModel.java
@@ -10,114 +10,99 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
 
 public class BotoModel<T extends BotoEntity> extends EntityModel<T> {
+
+    private final ModelRenderer head;
     private final ModelRenderer body;
     private final ModelRenderer tail;
     private final ModelRenderer tailFin;
+    private final ModelRenderer rightFin;
+    private final ModelRenderer leftFin;
+    private final ModelRenderer backFin;
 
     public BotoModel() {
-        this.textureWidth = 64;
-        this.textureHeight = 64;
-        float f = 18.0F;
-        float f1 = -8.0F;
-        this.body = new ModelRenderer(this, 22, 0);
-        this.body.addBox(-4.0F, -7.0F, 0.0F, 8.0F, 7.0F, 13.0F);
-        this.body.setRotationPoint(0.0F, 22.0F, -5.0F);
-        ModelRenderer modelrenderer = new ModelRenderer(this, 51, 0);
-        modelrenderer.addBox(-0.5F, 0.0F, 8.0F, 1.0F, 4.0F, 5.0F);
-        modelrenderer.rotateAngleX = ((float)Math.PI / 3F);
-        this.body.addChild(modelrenderer);
-        ModelRenderer modelrenderer1 = new ModelRenderer(this, 48, 20);
-        modelrenderer1.mirror = true;
-        modelrenderer1.addBox(-0.5F, -4.0F, 0.0F, 1.0F, 4.0F, 7.0F);
-        modelrenderer1.setRotationPoint(2.0F, -2.0F, 4.0F);
-        modelrenderer1.rotateAngleX = ((float)Math.PI / 3F);
-        modelrenderer1.rotateAngleZ = 2.0943952F;
-        this.body.addChild(modelrenderer1);
-        ModelRenderer modelrenderer2 = new ModelRenderer(this, 48, 20);
-        modelrenderer2.addBox(-0.5F, -4.0F, 0.0F, 1.0F, 4.0F, 7.0F);
-        modelrenderer2.setRotationPoint(-2.0F, -2.0F, 4.0F);
-        modelrenderer2.rotateAngleX = ((float)Math.PI / 3F);
-        modelrenderer2.rotateAngleZ = -2.0943952F;
-        this.body.addChild(modelrenderer2);
-        this.tail = new ModelRenderer(this, 0, 19);
-        this.tail.addBox(-2.0F, -2.5F, 0.0F, 4.0F, 5.0F, 11.0F);
-        this.tail.setRotationPoint(0.0F, -2.5F, 11.0F);
-        this.tail.rotateAngleX = -0.10471976F;
-        this.body.addChild(this.tail);
-        this.tailFin = new ModelRenderer(this, 19, 20);
-        this.tailFin.addBox(-5.0F, -0.5F, 0.0F, 10.0F, 1.0F, 6.0F);
-        this.tailFin.setRotationPoint(0.0F, 0.0F, 9.0F);
-        this.tailFin.rotateAngleX = 0.0F;
-        this.tail.addChild(this.tailFin);
-        ModelRenderer modelrenderer3 = new ModelRenderer(this, 0, 0);
-        modelrenderer3.addBox(-4.0F, -3.0F, -3.0F, 8.0F, 7.0F, 6.0F);
-        modelrenderer3.setRotationPoint(0.0F, -4.0F, -3.0F);
-        ModelRenderer modelrenderer4 = new ModelRenderer(this, 0, 13);
-        modelrenderer4.addBox(-1.0F, 2.0F, -7.0F, 2.0F, 2.0F, 4.0F);
-        modelrenderer3.addChild(modelrenderer4);
-        this.body.addChild(modelrenderer3);
+        textureWidth = 64;
+        textureHeight = 64;
+
+        // BODY
+        body = new ModelRenderer(this);
+        body.setRotationPoint(0.0F, 22.0F, 0.0F);
+        body.setTextureOffset(22, 0)
+                .addBox(-4.0F, -5.0F, -6.0F, 8, 7, 13);
+
+        // HEAD
+        head = new ModelRenderer(this);
+        head.setRotationPoint(0.0F, -2.0F, -6.0F);
+        body.addChild(head);
+        head.setTextureOffset(0, 0)
+                .addBox(-4.0F, -3.0F, -6.0F, 8, 7, 6);
+
+        // TAIL BASE
+        tail = new ModelRenderer(this);
+        tail.setRotationPoint(0.0F, -1.0F, 7.0F);
+        body.addChild(tail);
+        tail.setTextureOffset(0, 19)
+                .addBox(-2.0F, -2.5F, 0.0F, 4, 5, 11);
+
+        // TAIL FIN
+        tailFin = new ModelRenderer(this);
+        tailFin.setRotationPoint(0.0F, 0.0F, 11.0F);
+        tail.addChild(tailFin);
+        tailFin.setTextureOffset(19, 20)
+                .addBox(-5.0F, -0.5F, -1.0F, 10, 1, 6);
+
+        // RIGHT FIN
+        rightFin = new ModelRenderer(this);
+        rightFin.setRotationPoint(-4.0F, 0.0F, -2.0F);
+        body.addChild(rightFin);
+        rightFin.setTextureOffset(48, 20)
+                .addBox(-1.0F, 0.0F, 0.0F, 1, 4, 7);
+
+        // LEFT FIN
+        leftFin = new ModelRenderer(this);
+        leftFin.setRotationPoint(4.0F, 0.0F, -2.0F);
+        body.addChild(leftFin);
+        leftFin.setTextureOffset(48, 20)
+                .addBox(0.0F, 0.0F, 0.0F, 1, 4, 7);
+
+        // BACK FIN
+        backFin = new ModelRenderer(this);
+        backFin.setRotationPoint(0.0F, -5.0F, 2.0F);
+        body.addChild(backFin);
+        backFin.setTextureOffset(51, 0)
+                .addBox(-0.5F, 0.0F, 0.0F, 1, 4, 5);
     }
 
-    public Iterable<ModelRenderer> getParts() {
-        return ImmutableList.of(this.body);
-    }
-
-    /**
-     * Sets this entity's model rotation angles
-     */
     @Override
-    public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount,
+    public void setRotationAngles(T entity, float limbSwing, float limbSwingAmount,
                                   float ageInTicks, float netHeadYaw, float headPitch) {
 
-        // Reset rotations (important!)
-        this.body.rotateAngleX = 0.0F;
-        this.body.rotateAngleY = 0.0F;
-        this.tail.rotateAngleX = -0.1F;
-        this.tail.rotateAngleY = 0.0F;
-        this.tailFin.rotateAngleX = 0.0F;
+        // Reset rotations
+        body.rotateAngleX = 0;
+        body.rotateAngleY = 0;
+        tail.rotateAngleY = 0;
+        tailFin.rotateAngleY = 0;
 
-        // Head/body turning
-        this.body.rotateAngleY = netHeadYaw * ((float)Math.PI / 180F) * 0.5F;
+        // Head rotation
+        head.rotateAngleY = netHeadYaw * ((float)Math.PI / 180F);
+        head.rotateAngleX = headPitch * ((float)Math.PI / 180F);
 
-        boolean isMoving = Entity.horizontalMag(entityIn.getMotion()) > 1.0E-7D;
-        boolean inWater = entityIn.isInWater();
+        // Swimming animation
+        float swimSpeed = 0.4F;
+        float swimDegree = 0.6F;
 
-        if (inWater) {
+        body.rotateAngleY = MathHelper.cos(ageInTicks * swimSpeed) * swimDegree * 0.2F;
+        tail.rotateAngleY = MathHelper.cos(ageInTicks * swimSpeed) * swimDegree;
+        tailFin.rotateAngleY = MathHelper.cos(ageInTicks * swimSpeed) * swimDegree * 1.5F;
 
-            if (isMoving) {
-                // Smooth swimming wave
-                float swimSpeed = 0.4F;
-                float swimDegree = 0.6F;
-
-                this.body.rotateAngleY += MathHelper.cos(ageInTicks * swimSpeed) * 0.05F;
-
-                this.tail.rotateAngleY =
-                        MathHelper.cos(ageInTicks * swimSpeed) * swimDegree;
-
-                this.tailFin.rotateAngleY =
-                        MathHelper.cos(ageInTicks * swimSpeed + 0.5F) * swimDegree * 1.4F;
-            }
-            else {
-                // Idle floating motion
-                this.body.rotateAngleX =
-                        MathHelper.cos(ageInTicks * 0.1F) * 0.02F;
-
-                this.tail.rotateAngleY =
-                        MathHelper.cos(ageInTicks * 0.15F) * 0.1F;
-            }
-        }
-        else {
-            // Beached flop animation
-            this.body.rotateAngleZ =
-                    MathHelper.cos(ageInTicks * 0.8F) * 0.2F;
-
-            this.tail.rotateAngleY =
-                    MathHelper.cos(ageInTicks * 0.8F) * 0.6F;
-        }
+        // Fins subtle movement
+        rightFin.rotateAngleZ = -0.2F;
+        leftFin.rotateAngleZ = 0.2F;
     }
 
     @Override
-    public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
+    public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight,
+                       int packedOverlay, float red, float green, float blue, float alpha) {
 
+        body.render(matrixStack, buffer, packedLight, packedOverlay);
     }
 }
