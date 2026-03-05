6aab4db75c9d5976be0a1bdf881a5f2220f1bbb5
ADM Caio <caiodnobre@gmail.com>
2026-03-05 20:24:34 -0300

    Added the five first mobs.

--- src/main/java/com/brbiomesmod/item/ModItemGroup.java
+++ src/main/java/com/brbiomesmod/item/ModItemGroup.java
@@ -8,7 +8,7 @@ import net.minecraft.item.ItemStack;
 
 public class ModItemGroup {
 
-    public static final ItemGroup FOOD_GROUP = new ItemGroup("BrazilianBiomes:Food")
+    public static final ItemGroup FOOD_GROUP = new ItemGroup("brazilian_biomes_food")
     {
         @Override
         public ItemStack createIcon()
@@ -16,7 +16,7 @@ public class ModItemGroup {
             return new ItemStack(ModItems.ACAI_BOWL.get());
         }};
 
-    public static final ItemGroup FRUITS_AND_VEGETABLES_GROUP = new ItemGroup("BrazilianBiomes:Fruits&Vegetables")
+    public static final ItemGroup FRUITS_AND_VEGETABLES_GROUP = new ItemGroup("brazilian_biomes_fruits_vegetables")
     {
         @Override
         public ItemStack createIcon()
@@ -24,7 +24,7 @@ public class ModItemGroup {
             return new ItemStack(ModItems.PARANA_PINE_CONE.get());
         }};
 
-    public static final ItemGroup PLANTS_GROUP = new ItemGroup("BrazilianBiomes:Plants")
+    public static final ItemGroup PLANTS_GROUP = new ItemGroup("brazilian_biomes_plants")
     {
         @Override
         public ItemStack createIcon()
@@ -32,7 +32,7 @@ public class ModItemGroup {
             return new ItemStack(PlantsGroup.AMAZONIAN_ZEBRA_PLANT.get());
         }};
 
-    public static final ItemGroup WOOD_GROUP = new ItemGroup("BrazilianBiomes:Woods")
+    public static final ItemGroup WOOD_GROUP = new ItemGroup("brazilian_biomes_woods")
     {
         @Override
         public ItemStack createIcon()
@@ -40,7 +40,7 @@ public class ModItemGroup {
             return new ItemStack(WoodGroup.BALSA_PLANKS.get());
         }};
 
-    public static final ItemGroup TREE_GROUP = new ItemGroup("BrazilianBiomes:Trees")
+    public static final ItemGroup TREE_GROUP = new ItemGroup("brazilian_biomes_trees")
     {
         @Override
         public ItemStack createIcon()
@@ -48,7 +48,7 @@ public class ModItemGroup {
             return new ItemStack(TreesGroup.BRAZILWOOD_SAPLING.get());
         }};
 
-    public static final ItemGroup MISC_GROUP = new ItemGroup("BrazilianBiomes:Miscellaneous")
+    public static final ItemGroup MISC_GROUP = new ItemGroup("brazilian_biomes_miscellaneous")
     {
         @Override
         public ItemStack createIcon()
@@ -56,7 +56,7 @@ public class ModItemGroup {
             return new ItemStack(ModItems.BRAZILNUT_SHELL.get());
         }};
 
-    public static final ItemGroup CREATURES_GROUP = new ItemGroup("BrazilianBiomes:Creatures")
+    public static final ItemGroup CREATURES_GROUP = new ItemGroup("brazilian_biomes_creatures")
     {
         @Override
         public ItemStack createIcon()
