4b5234f0822822ff0a14064f5dafdddc067dc595
ADM Caio <caiodnobre@gmail.com>
2026-03-05 12:27:48 -0300

    Humboldt willow now generate in Amazon Varzea biome and can no-longer generate in Atlantic Forest biome, only in Serra do Mar Coastal Forest.

---
+++ src/main/java/com/brbiomesmod/entity/model/ManedWolfModel.java
@@ -0,0 +1,71 @@
+package com.brbiomesmod.entity.model;
+
+public class ManedWolfModel extends EntityModel<Entity> {
+    private final ModelRenderer head;
+    private final ModelRenderer body;
+    private final ModelRenderer leg1;
+    private final ModelRenderer leg2;
+    private final ModelRenderer leg3;
+    private final ModelRenderer leg4;
+    private final ModelRenderer tail;
+
+    public ManedWolfModel() {
+        textureWidth = 64;
+        textureHeight = 64;
+
+        head = new ModelRenderer(this);
+        head.setRotationPoint(-1.0F, 10.5F, -3.0F);
+        head.setTextureOffset(1, 5).addBox(-3.0F, -2.0F, -5.0F, 8.0F, 6.0F, 6.0F, 0.0F, false);
+        head.setTextureOffset(15, 1).addBox(3.0F, -4.0F, -4.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+        head.setTextureOffset(8, 1).addBox(-3.0F, -4.0F, -4.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+        head.setTextureOffset(6, 18).addBox(-1.0F, 2.0F, -8.0F, 4.0F, 2.0F, 3.0F, 0.0F, false);
+
+        body = new ModelRenderer(this);
+        body.setRotationPoint(0.0F, 16.0F, -6.0F);
+        setRotationAngle(body, 1.5708F, 0.0F, 0.0F);
+        body.setTextureOffset(24, 15).addBox(-3.0F, 4.0F, 1.5F, 6.0F, 11.0F, 6.0F, 0.0F, false);
+
+        leg1 = new ModelRenderer(this);
+        leg1.setRotationPoint(-5.0F, 17.5F, 7.0F);
+        leg1.setTextureOffset(13, 24).addBox(1.999F, -3.5F, -1.0F, 2.0F, 10.0F, 2.0F, 0.0F, false);
+
+        leg2 = new ModelRenderer(this);
+        leg2.setRotationPoint(-1.0F, 17.5F, 7.0F);
+        leg2.setTextureOffset(4, 24).addBox(2.001F, -3.5F, -1.0F, 2.0F, 10.0F, 2.0F, 0.0F, false);
+
+        leg3 = new ModelRenderer(this);
+        leg3.setRotationPoint(-5.0F, 17.5F, 0.0F);
+        leg3.setTextureOffset(13, 24).addBox(1.999F, -3.5F, -1.0F, 2.0F, 10.0F, 2.0F, 0.0F, false);
+
+        leg4 = new ModelRenderer(this);
+        leg4.setRotationPoint(-1.0F, 17.5F, 0.0F);
+        leg4.setTextureOffset(4, 24).addBox(2.001F, -3.5F, -1.0F, 2.0F, 10.0F, 2.0F, 0.0F, false);
+
+        tail = new ModelRenderer(this);
+        tail.setRotationPoint(-4.0F, 17.0F, 9.0F);
+        setRotationAngle(tail, 1.5708F, 0.0F, 0.0F);
+        tail.setTextureOffset(19, 33).addBox(3.0F, 0.0F, -6.0F, 2.0F, 2.0F, 12.0F, 0.0F, false);
+    }
+
+    @Override
+    public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
+        //previously the render function, render code was moved to a method below
+    }
+
+    @Override
+    public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+        head.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+        body.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+        leg1.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+        leg2.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+        leg3.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+        leg4.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+        tail.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+    }
+
+    public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+        modelRenderer.rotateAngleX = x;
+        modelRenderer.rotateAngleY = y;
+        modelRenderer.rotateAngleZ = z;
+    }
+}
