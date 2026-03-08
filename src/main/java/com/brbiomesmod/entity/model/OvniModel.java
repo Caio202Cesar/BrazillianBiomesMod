8fd98e6c34085aadc5afb9da39e24ab0c7c3d671
ADM Caio <caiodnobre@gmail.com>
2026-03-07 23:35:01 -0300

    Added the five first mobs.

---
+++ src/main/java/com/brbiomesmod/entity/model/OvniModel.java
@@ -0,0 +1,42 @@
+package com.brbiomesmod.entity.model;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+
+public class OvniModel extends EntityModel<Entity> {
+    private final ModelRenderer bb_main;
+
+    public OvniModel() {
+        texWidth = 256;
+        texHeight = 256;
+
+        bb_main = new ModelRenderer(this);
+        bb_main.setPos(0.0F, 24.0F, 0.0F);
+        bb_main.texOffs(0, 0).addBox(-17.0F, -8.0F, -17.0F, 35.0F, 2.0F, 35.0F, 0.0F, false);
+        bb_main.texOffs(0, 37).addBox(-14.0F, -6.0F, -14.0F, 29.0F, 2.0F, 29.0F, 0.0F, false);
+        bb_main.texOffs(0, 68).addBox(-14.0F, -10.0F, -14.0F, 29.0F, 2.0F, 29.0F, 0.0F, false);
+        bb_main.texOffs(0, 99).addBox(-8.0F, -4.0F, -8.0F, 16.0F, 3.0F, 16.0F, 0.0F, false);
+        bb_main.texOffs(116, 37).addBox(-5.0F, -17.0F, -5.0F, 10.0F, 5.0F, 10.0F, 0.0F, false);
+        bb_main.texOffs(64, 99).addBox(-8.0F, -13.0F, -8.0F, 16.0F, 3.0F, 16.0F, 0.0F, false);
+        bb_main.texOffs(116, 52).addBox(-5.0F, -2.0F, -5.0F, 10.0F, 2.0F, 10.0F, 0.0F, false);
+    }
+
+    @Override
+    public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
+        //previously the render function, render code was moved to a method below
+    }
+
+    @Override
+    public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+        bb_main.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+    }
+
+    public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+        modelRenderer.xRot = x;
+        modelRenderer.yRot = y;
+        modelRenderer.zRot = z;
+    }
+}
