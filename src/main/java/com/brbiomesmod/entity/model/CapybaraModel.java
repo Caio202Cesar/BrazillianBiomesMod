62da4f63e706c8e7c262176295a838212f691bcb
ADM Caio <caiodnobre@gmail.com>
2026-04-06 04:17:02 -0300

    Added rubberized pickaxes and shovels, which have silk touch without need for enchantment.

--- src/main/java/com/brbiomesmod/entity/model/CapybaraModel.java
+++ src/main/java/com/brbiomesmod/entity/model/CapybaraModel.java
@@ -23,24 +23,24 @@ public class CapybaraModel<T extends CapybaraEntity> extends EntityModel<T> {
         textureHeight = 64;
 
         body = new ModelRenderer(this);
-        body.setRotationPoint(0.0F, 22.0F, 0.0F);
-        body.setTextureOffset(0, 0).addBox(-4.0F, -9.0F, -5.0F, 6.0F, 7.0F, 11.0F, 0.0F, false);
+        body.setRotationPoint(0.0F, 24.0F, 0.0F);
+        body.setTextureOffset(0, 0).addBox(-4.0F, -8.0F, -5.0F, 6.0F, 7.0F, 11.0F, 0.0F, false);
 
         legFL = new ModelRenderer(this, 0, 18);
-        legFL.setRotationPoint(0.0F, 22.0F, 0.0F);
-        legFL.setTextureOffset(0, 30).addBox(0.0F, -2.0F, -4.0F, 2, 3, 2, 0, false);
+        legFL.setRotationPoint(0.0F, 23.0F, 0.0F);
+        legFL.setTextureOffset(0, 30).addBox(0.0F, -2.0F, -4.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
 
         legFR = new ModelRenderer(this, 0, 18);
-        legFR.setRotationPoint(0.0F, 22.0F, 0.0F);
-        legFR.setTextureOffset(22, 26).addBox(-4.0F, -2.0F, -4.0F, 2, 3, 2, 0, false);
+        legFR.setRotationPoint(0.0F, 23.0F, 0.0F);
+        legFR.setTextureOffset(22, 26).addBox(-4.0F, -2.0F, -4.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
 
         legBL = new ModelRenderer(this, 0, 18);
-        legBL.setRotationPoint(0.0F, 22.0F, 0.0F);
-        legBL.setTextureOffset(22, 22).addBox(0.0F, -2.0F, 4.0F, 2, 3, 2, 0, false);
+        legBL.setRotationPoint(0.0F, 23.0F, 0.0F);
+        legBL.setTextureOffset(22, 22).addBox(0.0F, -2.0F, 4.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
 
         legBR = new ModelRenderer(this, 0, 18);
-        legBR.setRotationPoint(0.0F, 22.0F, 0.0F);
-        legBR.setTextureOffset(22, 18).addBox(-4.0F, -3.0F, 4.0F, 2, 3, 2, 0, false);
+        legBR.setRotationPoint(0.0F, 23.0F, 0.0F);
+        legBR.setTextureOffset(22, 18).addBox(-4.0F, -2.0F, 4.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
 
         head = new ModelRenderer(this, 0, 0);
         head.setRotationPoint(0.0F, 16.0F, -7.0F);
