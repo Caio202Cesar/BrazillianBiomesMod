e4f525c3f20c065dd203b3b9d3e1681a9e6dc7d9
Caio202Cesar <caiodnobre@gmail.com>
2026-05-23 19:25:56 -0300

    Changed umbu tree size and shape.

--- src/main/java/com/brbiomesmod/BrazillianBiomesMod.java
+++ src/main/java/com/brbiomesmod/BrazillianBiomesMod.java
@@ -114,9 +114,7 @@ public class BrazillianBiomesMod {
                     .put(TreesGroup.UMBU_LOG.get(), TreesGroup.STRIPPED_UMBU_LOG.get())
                     .put(TreesGroup.WOLF_APPLE_LOG.get(), TreesGroup.STRIPPED_WOLF_APPLE_LOG.get())
                     .put(TreesGroup.JENIPAPO_LOG.get(), Blocks.STRIPPED_JUNGLE_LOG)
-                    .put(TreesGroup.YERBA_MATE_LOG.get(), TreesGroup.STRIPPED_YERBA_MATE_LOG.get())
-
-                    .build();
+                    .put(TreesGroup.YERBA_MATE_LOG.get(), TreesGroup.STRIPPED_YERBA_MATE_LOG.get()).build();
 
             ModBiomeGeneration.generateBiomes();
 
