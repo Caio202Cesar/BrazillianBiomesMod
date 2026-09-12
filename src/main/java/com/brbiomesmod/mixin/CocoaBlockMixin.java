c88c42adecc3964a15e24a4fbd2f0b78a5a06e99
Caio202Cesar <caiodnobre@gmail.com>
2026-09-12 03:44:43 -0300

    Added cocoa fruit, which can be collected from pods instead of the beans. These fruits can be crafted in four beans.

---
+++ src/main/java/com/brbiomesmod/mixin/CocoaBlockMixin.java
@@ -0,0 +1,30 @@
+package com.brbiomesmod.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CocoaBlock;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static net.minecraft.state.properties.BlockStateProperties.HORIZONTAL_FACING;
+
+@Mixin(CocoaBlock.class)
+public class CocoaBlockMixin {
+    @Inject(
+            method = "isValidPosition",
+            at = @At("HEAD")
+    )
+
+    public void isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        Block block = worldIn.getBlockState(pos.offset(state.get(HORIZONTAL_FACING))).getBlock();
+
+        return WoodGroup.CACAO_LOG;
+    }
+}
+
+
